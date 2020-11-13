3a33301a6716deb4957ad7f27c4154a706da8904
@RubenGarciaOrdaz <rgo1999@hotmail.com>
2020-11-13 13:58:45 -0600

    Actualizamos programa de instalacion

--- servidor_dns.cpp
+++ servidor_dns.cpp
@@ -5,21 +5,47 @@ using namespace std;
 void capturarDatos();
 void archivoDNS();
 void archivoDNSReverso();
+void crearArchivoDHCPv4();
+void crearArchivoDHCPv6();
 void namedFile();
 void instrucciones();
-void instalator();
+void installer();
+void crearArchivoApgadoReinicio();
 void action(const string s);
 
-string dominio, ip2="", ip, reverse_ip, hostname, narch, command, porcion_host;
+string dominio, ip2="", ip, reverse_ip, hostname, narch, porcion_red="", porcion_host="", interfaz="", rango_inferior="", rango_superior="";
 int main(){
 
+	crearArchivoApgadoReinicio();
+	system("rm *.db");
+	system("rm *.conf");
+	capturarDatos();
+	archivoDNS();
+	archivoDNSReverso();
+	namedFile();
+	crearArchivoDHCPv4();
+	crearArchivoDHCPv6();
 
+	cout << hostname << endl;
+	cout << ip << endl;
+	cout << reverse_ip << endl;
+	cout << porcion_host << endl;
+	cout << porcion_red << endl;
+	cout << interfaz << endl;
+	cout << rango_inferior << endl;
+	cout << rango_superior << endl;
 	
+
 	
 	return 0;
 }
 void installer(){
-	//Creamos los archivos correspondientes
+
+	//Habilitamos SSH
+	system("svcadm enable ssh");
+	system("pkg update");
+	
+	cout << "Generando archivos de configuracion necesarios...." << endl << "Rellene los campos del formulario..." << endl;
 	capturarDatos();
 	archivoDNS();
 	action("ls -l");
@@ -27,6 +53,18 @@ void installer(){
 	action("ls -l");
 	namedFile();
 	action("ls -l");
+	system("clear");
+	ofstream arch("dhcpd6.conf");
+	getline(cin, interfaz, '\n');
+	
+
+
+	
+
+
+	//Instalamos el servicio de DNS
+	action("package install pkg install service/network/dns/bind");
+	//Creamos los archivos correspondientes
 
 	//Guardamos el nombre de nuestros archivos
 	char rpt;
@@ -44,7 +82,7 @@ void installer(){
 	action("mv " + filename2 + "/etc/namedb/master/");
 
 	//Validamos que la configuracion de nuestros archivos funcionen
-	cout << "Moviendo el archivo de configuracion a /Etc/" << endl;
+	cout << "Moviendo el archivo de configuracion a /etc/" << endl;
 	action("named-checkconf /etc/named.conf");
 	cout << "Moviendo archivo de configuracion DNS a /var/namedb/master" << endl;
 	action("named-checkzone " + dominio + "/var/namedb/master/"+filename1);
@@ -55,8 +93,18 @@ void installer(){
 	cin >> rpt;
 	if(!(rpt == 's' || rpt == 'S')){
 		exit(0);
-	}
+	}else{
+		cout << "Habilitando servicio DNS... " << endl;
+		cout << "Refrescando el servicio DNS..." << endl;
+		action("svcadm refresh dns/server");
+		cout << "Habilitando el servicio DNS..." << endl;
+		action("svcadm enable dns/server");
+		cout << "Mostrando el status del servicio..." << endl;
+		action("svcs dns/server");
 
+
+	}
+	
 	//Agregamos a nuestro cliente dns local nuestro servidor dns
 	cout << "Habilitando su servidor DNS en el cliente del host local..." << endl;
 	action("svccfg -s network/dns/client setprop config/nameserver = net_address: " + ip);
@@ -68,37 +116,48 @@ void installer(){
 
 }
 void capturarDatos(){
+	//Este metodo solo sirve para redes ccon una IP clase C con una submascara de red /24
 	cout << "Digite su dominio: "; getline(cin, dominio, '\n');
 	cout << "Digite la ip de su servidor DNS: "; getline(cin, ip, '\n');
 	cout << "Digite el nombre de su hostname: "; getline(cin, hostname, '\n');
-	cout << "Digite la porcion de host: "; getline(cin, porcion_host, '\n');
-}
-void archivoDNSReverso(){
-	string n[3];
+	cout << "Digite el nombre de la interfaz: "; getline(cin, interfaz, '\n');
+	cout << "Digite el rango inferior de su servidor DHCP: "; getline(cin, rango_inferior, '\n');
+	cout << "Digite el rango superior de su servidor DHCP: "; getline(cin, rango_superior, '\n');
+	string n[4]; //Arreglo para guardar los digitos de la ip inversa
 	string var = "";
 	for(int i = 0, cont = 0; i<ip.length(); i++){
-		if(cont<3){
-			if(ip[i]=='.' && cont<3){
+		if(cont<4){
+			if(ip[i]=='.' && cont<4){
 				n[cont] = var;
 				var="";
 				cont++;
-
-			}else if(cont<3){
-				var+=ip[i];
-			}else
-			{
-				break;
 			}
+			if(ip[i]!='.' && ip[i]!='\n'){
+				var+=ip[i];
+			}	
 		}
 	}
+	porcion_host = var;
+	var = "";
+	for(int i = 0; i<3; i++){
+		porcion_red += n[i];
+		if ( i != 2) {
+			porcion_red +=".";
+		}
+	}
+	cout << "Por host: " << porcion_host << endl;
 	var="";
 	for(int i = 2; i>=0; i--){
 		var+=n[i];
 		if(i!=0)
 			var+=".";
 	}
-	ip2 = var;
-	narch = ip2 + ".db";
+	reverse_ip = var;
+}
+void archivoDNSReverso(){
+
+	//Se genera el archivo reverso
+	narch = reverse_ip + ".db";
 	ofstream arch(narch.c_str());
 	if(arch.is_open()){
 		arch << "$TTL 3h" << endl;
@@ -106,16 +165,16 @@ void archivoDNSReverso(){
 		arch << "        2013022744" << endl  << "        28800" << endl << "        3600" << endl;
 		arch << "        604800" << endl << "        38400" << endl << ")" << endl;
 		arch << "@" << "\t\tIN\t\tNS\t\t" << hostname << "." << dominio << "." << endl;
-		arch << "@" << "\t\tIN\t\tPTR\t\t" << ip << "." << endl;
+		arch << "@" << "\t\tIN\t\tPTR\t\t" << ip << endl;
 		arch << "DNS" << "\t\tIN\t\tA\t\t" << ip << endl;
 		arch << hostname << "\t\tIN\t\tA\t\t" << ip << endl;
 		arch << porcion_host << "\t\tIN\t\tPTR\t\t" << hostname << "." << dominio;
 	}
 	arch.close();
-	getchar();
 }
 void archivoDNS()
 {
+	//Se genera el archvio de dominio para el dns
 	narch = dominio + ".db";
 	ofstream arch(narch.c_str());
 	if(arch.is_open()){
@@ -124,11 +183,11 @@ void archivoDNS()
 		arch << "        2013022744" << endl  << "        28800" << endl << "        3600" << endl;
 		arch << "        604800" << endl << "        38400" << endl << ")" << endl;
 		arch << dominio << ".\t\tIN\t\tNS\t\t" << hostname << "." << dominio << "." << endl;
-		arch << hostname << "\t\tIN\tA\t" << ip << endl;
+		arch << hostname << "\t\tIN\t\tA\t\t" << ip << endl;
 	}
 	arch.close();
-	getchar();
 }
+
 void namedFile(){
 	narch = "named.conf";
 	ofstream arch(narch.c_str());
@@ -147,15 +206,60 @@ void namedFile(){
 		arch << "};" << endl;
 
 		arch << "zone \"" << reverse_ip << ".in-addr.arpa\" {" << endl;
-		arch << "type master;" << endl;
-		arch << "file \"/etc/namedb/master/" << reverse_ip << ".db\";" << endl;
+		arch << "\ttype master;" << endl;
+		arch << "\tfile \"/etc/namedb/master/" << reverse_ip << ".db\";" << endl;
 		arch << "};" << endl;
 		arch.close();
 		getchar();
 	}
 }
+
+void crearArchivoDHCPv6()
+{
+	ofstream arch("dhcp6.conf");
+	if(arch.good()){
+		arch << "subnet6 2001:db8:0:1::/64{" << endl;
+		arch << "\tinterface " << interfaz<< ";" << endl;
+		arch << "\trange6 2001:db8:0:1::120 2001:db8:0:1::200;" << endl;
+		arch << "}";
+		arch.close();
+	}
+}
+void crearArchivoDHCPv4(){
+	ofstream file("dhcpd4.conf");
+	if(file.good()){
+		
+		file << "subnet " << porcion_host +".0" << " netmask 255.255.255"  << '{' << endl;
+		file << "interface " << interfaz << "; " << endl;
+		file << "range " << rango_inferior << " " << rango_superior << "; " << endl;
+		file << "option routers " << porcion_host+".0" << "; " << endl;
+		file << "option broadcast-address " << porcion_host +".255" << "; " <<endl;
+		file <<'}' << '\n' << endl;
+		file.close();
+	}
+}
+
 void action(const string s){
 
 	system(s.c_str());
 	getchar();
+}
+
+void crearArchivoApagadoReinicio(){
+
+	cout << "Creando archivo de apagado...." << endl;
+	ofstream file("shutdown.sh");
+	if(file.is_open()){
+		file << "#!/usr/bin/env bash" << endl;
+		file << "shutdown -y -g10 -i5" << endl;
+		file.close();
+	}
+	file.open("reboot.sh");
+	if(file.is_open()){
+		file << "#!/usr/bin/env bash" << endl;
+		file << "shutdown -y -g10 -i6" << endl;
+		file.close();
+	}
+
+
 }
